602c9b550e2dfac544eeb0023a63a5871fce5163
John Peebles <jpeeblesg@gmail.com>
2014-08-06 00:14:15 -0400

    actualicaciones a gemfile mas reciente

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,8 +6,8 @@ module Api
       before_filter :restrict_access
       def restrict_access
         access_token=request.headers["HTTP_ACCESS_TOKEN"]||params[:access_token]
-        email=request.headers["HTTP_EMAIL"]
-        password=request.headers["HTTP_PASSWORD"]
+        email=request.headers["HTTP_EMAIL"]||params[:email]
+        password=request.headers["HTTP_PASSWORD"]||params[:password]
         # if params[:access_token]!=nil
         if access_token!=nil
           api_key = ApiKey.find_by_access_token(access_token)
@@ -30,7 +30,7 @@ module Api
       end
         # respond_with @user.get_bookmarks.map{|b| JSON.parse(b.to_json)}
       def bookmarks
-        @user.bookmarks_as_json
+        respond_with @user.bookmarks_as_json
       end
       def show
         respond_with @user ? User.find(@user.id) : User.find_by_email(params[:id])
@@ -39,6 +39,9 @@ module Api
         g=Group.where(name: params[:name],user_id: @user.id).first_or_create
         UserGroup.where(user_id: g.user_id, group_id: g.id).first_or_create
         respond_with g
+      end
+      def new_folder
+        
       end
 
     end
