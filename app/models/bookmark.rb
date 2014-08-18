a5df66abdaf7c4c22f20c7b93e9a854c6bb9e46c
John Peebles <jpeeblesg@gmail.com>
2014-08-18 09:54:30 -0400

    This is supposed to crash

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,7 @@ class Bookmark < ActiveRecord::Base
 	belongs_to :folder
 	belongs_to :group
 	belongs_to :user
-	after_save :check_link
+	before_save :check_link
 	def check_link
 		unless self.link[0..3]=="http"
 			self.link="http://"+self.link
