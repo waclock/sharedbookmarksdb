7f3819d0bf2eb6a7cd58094c3e39d8dc5244fa68
John Peebles <jpeeblesg@gmail.com>
2014-09-06 16:42:56 -0400

    added chrome-extension link

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,7 +4,7 @@ class Bookmark < ActiveRecord::Base
 	belongs_to :user
 	before_save :check_link
 	def check_link
-		unless self.link[0..3]=="http"
+		unless self.link[0..3]=="http" || self.include? "chrome-extension://" 
 			self.link="http://"+self.link
 		end
 	end
